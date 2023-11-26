619ec1ebed56333e06fe50a607ff1c0d588e3dc5
Jabra Ram <jrchoudhary2410@gmail.com>
2023-11-26 15:08:51 +0530

    add experience section

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const descriptions = document.querySelectorAll('.about__desc')
-
+  const descriptions = document.querySelectorAll('.hero__desc');
+  const theme = document.getElementById('theme');
+  const themeMobile = document.getElementById('theme-mobile');
+  console.log(themeMobile)
   const intial_bg = getComputedStyle(document.documentElement).getPropertyValue('--background')
   if (intial_bg == '#e5e5e5') {
     theme.classList.add('light');
     theme.checked = true;
+    themeMobile.classList.add('light');
+    themeMobile.checked = true;
   }
 
   theme.addEventListener('click', () => {
     theme.classList.toggle('light');
     if (theme.classList.contains('light')) {
+      document.documentElement.style.setProperty('--background', '#e5e5e5');
+      document.documentElement.style.setProperty('--foreground', '#000000');
+    }
+    else {
+      document.documentElement.style.setProperty('--background', '#000000');
+      document.documentElement.style.setProperty('--foreground', '#e5e5e5');
+    }
+  })
+
+  themeMobile.addEventListener('click', () => {
+    themeMobile.classList.toggle('light');
+    if (themeMobile.classList.contains('light')) {
       document.documentElement.style.setProperty('--background', '#e5e5e5');
       document.documentElement.style.setProperty('--foreground', '#000000');
     }
